da1099b405c6d25b3f5791f43c49ec76745e246b
Akash Patel <akash_patel97@hotmail.com>
2016-08-01 22:03:11 -0400

    changed to function

--- quotes.js
+++ quotes.js
@@ -1,26 +1,28 @@
-  $(document).ready(function () {
-      var twitterLink = "https://twitter.com";
+var getNewQuote = function () {
+    $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+        type: 'POST',
+        data: {
+            'X-Mashape-Key': '3VLdbhpG6KmshFZuDzT0UWaVxR40p1jNuXIjsn1BWJZHA8hcnC',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        },
+        dataType: "json",
+        success: function (jsonData) {
+            console.log(jsonData);
+            $("#quote-text").html('<p>"' + jsonData.quote + '"</p>');
+            $(".quote-source").html('<i>' + jsonData.author + '</i>');
+            twitterLink = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + jsonData.quote + '" ' + jsonData.author);
+            $("#shareTwitter").attr('href', twitterLink);
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "I01Di6UpMLmshSb4O2Y1gjKzPInKp1snSzejsnPFVOaLk74izr");
+        }
+    });
+};
 
-      $("#new-quote").on("click", function () {
-          $.ajax({
-              url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-              type: 'POST',
-              data: {
-                  'X-Mashape-Key': '3VLdbhpG6KmshFZuDzT0UWaVxR40p1jNuXIjsn1BWJZHA8hcnC',
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                  'Accept': 'application/json'
-              },
-              dataType: "json",
-              success: function (jsonData) {
-                  console.log(jsonData);
-                  $("#quote-text").html('<p>"' + jsonData.quote + '"</p>');
-                  $(".quote-source").html('<i>' + jsonData.author + '</i>');
-                  twitterLink = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + jsonData.quote + '" ' + jsonData.author);
-                  $("#shareTwitter").attr('href', twitterLink);
-              },
-              beforeSend: function (xhr) {
-                  xhr.setRequestHeader("X-Mashape-Authorization", "I01Di6UpMLmshSb4O2Y1gjKzPInKp1snSzejsnPFVOaLk74izr");
-              }
-          });
-      });
-  });
+$(document).ready(function () {
+    getNewQuote();
+    var twitterLink = "https://twitter.com";
+    $("#new-quote").on("click", getNewQuote);
+});
