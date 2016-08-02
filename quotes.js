4a11c797358ab86a410735ba6c827e95e79dc22a
Akash Patel <akash_patel97@hotmail.com>
2016-08-01 21:53:42 -0400

    twitter sharing

--- quotes.js
+++ quotes.js
@@ -1,4 +1,6 @@
   $(document).ready(function () {
+      var twitterLink = "https://twitter.com";
+
       $("#new-quote").on("click", function () {
           $.ajax({
               url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
@@ -12,7 +14,9 @@
               success: function (jsonData) {
                   console.log(jsonData);
                   $("#quote-text").html('<p>"' + jsonData.quote + '"</p>');
-                  $(".quote-source").html('<i> -' + jsonData.author + '</i>');
+                  $(".quote-source").html('<i>' + jsonData.author + '</i>');
+                  twitterLink = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + jsonData.quote + '" ' + jsonData.author);
+                  $("#shareTwitter").attr('href', twitterLink);
               },
               beforeSend: function (xhr) {
                   xhr.setRequestHeader("X-Mashape-Authorization", "I01Di6UpMLmshSb4O2Y1gjKzPInKp1snSzejsnPFVOaLk74izr");
